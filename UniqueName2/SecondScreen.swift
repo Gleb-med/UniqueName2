9a42696d7bf4903447da85b48bb1332353d0872e
Глеб Рахимжанов <glebrahimzanov@MacBook-Air-Gleb.local>
2022-05-18 01:28:30 +0300

    all screens loaded

---
+++ UniqueName2/SecondScreen.swift
@@ -0,0 +1,29 @@
+//
+//  SecondScreen.swift
+//  UniqueName2
+//
+//  Created by Глеб Рахимжанов on 18.05.2022.
+//
+
+import UIKit
+
+class SecondScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
